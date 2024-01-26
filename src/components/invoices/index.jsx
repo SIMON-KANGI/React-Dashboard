2aeac7718a22ceafe0842829f512cc70ce4d477e
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-01-26 19:34:28 +0300

    added invoices

--- src/components/invoices/index.jsx
+++ src/components/invoices/index.jsx
@@ -1,10 +1,78 @@
-
-
+import { DataGrid } from "@mui/x-data-grid"
+import { mockDataInvoices } from "../../data/mockData";
+import { Box, Typography } from "@mui/material";
 function Invoices() {
+  const Columns=[
+  {field: "id", headerName: "ID", flex: 0.5, cellClassName:"id-column-cell"},
+      {field: "name", headerName: "Name", flex: 1, cellClassName: "name-column-cell"},
+      {field: "phone", headerName: "Phone Number", cellClassName:"id-column-cell"},
+      {field: "email", headerName: "Email", cellClassName:"id-column-cell"},
+      {field: "cost", headerName: "Cost", cellClassName:"name-column-cell"},
+      {field: "date", headerName: "Date", cellClassName:"id-column-cell"}
+  ];
+  
   return (
-    <div>
-      <h1>Invoices</h1>
-    </div>
+    <Box>
+    <Typography variant="h4">
+      INVOICES
+    </Typography>
+    <Box
+height="65vh"
+backgroundColor="rgb(18,47,73)"
+border="solid 0.3px rgb(40,20,20)"
+sx={{
+"& .id-column-cell":{
+border:"solid 0.3px rgb(40,20,20)",
+backgroundColor:"rgb(18,47,73)",
+color:"white",
+padding:"1rem",
+fontSize:"12px"
+
+},
+"& .name-column-cell":{
+border:"solid 0.3px rgb(40,20,20)",
+backgroundColor:"rgb(18,47,73)",
+color:"rgb(1,220,190)",
+fontSize:"12px"
+
+},
+"& .MuiDataGrid-columnHeaders": {
+                    backgroundColor:"rgb(48,47,135)",
+                    borderBottom: "none",
+                    color:"white",
+                    fontSize:"12px"
+                    
+  },
+  "& .MuiDataGrid-footerContainer": {
+                    borderTop: "none",
+                    backgroundColor: "rgb(48,47,135)",
+                    color:"white"
+                },
+
+    "& .PrivateSwitchBase-input": {
+      backgroundColor:"rgb(1,220,190)",
+      border:"solid 0.3px rgb(40,20,20)"
+                },   
+}}
+
+
+    >
+      <DataGrid
+columns={Columns}
+rows={mockDataInvoices}
+initialState={{
+          pagination: {
+            paginationModel: { page: 0, pageSize: 8 },
+          },
+        }}
+        pageSizeOptions={[8, 10]}
+        checkboxSelection
+
+      />
+       </Box>
+
+  
+    </Box>
   )
 }
 
--- src/data/mockData.js
+++ src/data/mockData.js
@@ -29,33 +29,32 @@ icon:SupervisorAccountOutlinedIcon
             level:"Manager",
             icon:AdminPanelSettingsOutlinedIcon
                 },
-                {
-                  id:4,
-                  name:'Samson Kmata',
-                  age:34,
-                  number:"(254)794039988",
-                  email:"sammyk@gmail.com",
-                  level:"manager",
-                  icon:AdminPanelSettingsOutlinedIcon
+           {
+             id:4,
+             name:'Samson Kmata',
+             age:34,
+             number:"(254)794039988",
+              email:"sammyk@gmail.com",
+               level:"manager",
+               icon:AdminPanelSettingsOutlinedIcon
                       },
-                      {
-                        id:5,
-                        name:'Derrick Mwangi',
-                        age:30,
-                        number:"(254)793093488",
-                        email:"deroM@gmail.com",
-                        level:"user",
-                        icon:VerifiedUserOutlinedIcon
+               {
+                id:5,
+                name:'Derrick Mwangi',
+                age:30,
+                number:"(254)793093488",
+                email:"deroM@gmail.com",
+                level:"user",
+                icon:VerifiedUserOutlinedIcon
                             },
-                            {
-                              id:6,
-                              name:'Mestos Mariuki',
-                              age:26,
-                              number:"(254)799488488",
-                              email:"mestosm@gmail.com",
-                              level:"manager",
-                              icon:AdminPanelSettingsOutlinedIcon
-                                  },
+               {
+               id:6,
+                 name:'Mestos Mariuki',
+                 age:26,
+                number:"(254)799488488",
+              email:"mestosm@gmail.com",                              level:"manager",
+              icon:AdminPanelSettingsOutlinedIcon                                                      
+                 },
                                   {
                                     id:7,
                                     name:'kimani Chalo',
@@ -75,7 +74,7 @@ icon:SupervisorAccountOutlinedIcon
                                             icon:SupervisorAccountOutlinedIcon
                                                 }
 
-  ]
+  ]   
   export const ContactsTeam=[
     {
 id:1,
@@ -162,14 +161,80 @@ city:"Nairobi"
                                             city:"Chicago"
                                                 },
                                                 {
-                                                  id:9,
-                                                  name:'Gedion Mwarimbo',
-                                                  age:28,
-                                                  number:"(254)7309492488",
-                                                  email:"gidimwa@gmail.com",
-                                                  registerId:63273,
-                                                  zip:21003,
-                                                  city:"Melborne"
+       id:9,
+      name:'Gedion Mwarimbo',
+       age:28,
+      number:"(254)7309492488",
+      email:"gidimwa@gmail.com",
+      registerId:63273,
+        zip:21003,
+    city:"Melborne"
                                                       }
 
-  ]
+  ];
+  export const mockDataInvoices = [
+    {
+      id: 1,
+      name: "Jon Snow",
+      email: "jonsnow@gmail.com",
+      cost: "21.24",
+      phone: "(665)121-5454",
+      date: "03/12/2022",
+    },
+    {
+      id: 2,
+      name: "Cersei Lannister",
+      email: "cerseilannister@gmail.com",
+      cost: "1.24",
+      phone: "(421)314-2288",
+      date: "06/15/2021",
+    },
+    {
+      id: 3,
+      name: "Jaime Lannister",
+      email: "jaimelannister@gmail.com",
+      cost: "11.24",
+      phone: "(422)982-6739",
+      date: "05/02/2022",
+    },
+    {
+      id: 4,
+      name: "Anya Stark",
+      email: "anyastark@gmail.com",
+      cost: "80.55",
+      phone: "(921)425-6742",
+      date: "03/21/2022",
+    },
+    {
+      id: 5,
+      name: "Daenerys Targaryen",
+      email: "daenerystargaryen@gmail.com",
+      cost: "1.24",
+      phone: "(421)445-1189",
+      date: "01/12/2021",
+    },
+    {
+      id: 6,
+      name: "Ever Melisandre",
+      email: "evermelisandre@gmail.com",
+      cost: "63.12",
+      phone: "(232)545-6483",
+      date: "11/02/2022",
+    },
+    {
+      id: 7,
+      name: "Ferrara Clifford",
+      email: "ferraraclifford@gmail.com",
+      cost: "52.42",
+      phone: "(543)124-0123",
+      date: "02/11/2022",
+    },
+    {
+      id: 8,
+      name: "Rossini Frances",
+      email: "rossinifrances@gmail.com",
+      cost: "21.24",
+      phone: "(222)444-5555",
+      date: "05/02/2021",
+    },
+  ];
